881f30d8cf0b631af0ef672dc79938558d20b7c7
adhamnugroho <76804759+adhamnugroho@users.noreply.github.com>
2021-09-08 22:14:08 +0700

    Menggganti title
    Mengganti isi dari tag tittle yang sebelumnya "Soal Nomor 2_Method POST", menjadi "Soal Nomor 1_Method POST".

--- 02_Tugas Method POST and GET/method_post/soal nomor 1/index.php
+++ 02_Tugas Method POST and GET/method_post/soal nomor 1/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Soal Nomor 2_Method Post</title>
+	<title>Soal Nomor 1_Method Post</title>
 </head>
 <body>
 
