33b6aadee3ddb191851b3f7996b4768e6fe3b2a7
adhamnugroho <76804759+adhamnugroho@users.noreply.github.com>
2021-09-11 10:37:46 +0700

    Menmabahkan repo tugas web dinamis
    Menambahkan repo tugas web dinamis , yang didalamnya ada sub repo, dan ada 2 file. Pertama file "activity_layout_table.xml", kedua file "'Layout_Table.java".

---
+++ Tugas Web Dinamis/Tugas 6 - Membuat Table Layout/Layout Table/src/com/layout/tabel/Layout_Tabel.java
@@ -0,0 +1,14 @@
+package com.layout.tabel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Layout_Tabel extends Activity {
+	
+	//	Called when the activity is first created.
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__tabel);
+    }
+}
