18e9c882a900107a9aace981ea78e8cd53c4bbe4
adhamnugroho <76804759+adhamnugroho@users.noreply.github.com>
2021-09-11 10:40:39 +0700

    Menambahkan "@Override", dan menambahkan sedikit penjelasan.

--- Tugas Web Dinamis/Tugas 6 - Membuat Table Layout/Layout Table/src/com/layout/tabel/Layout_Tabel.java
+++ Tugas Web Dinamis/Tugas 6 - Membuat Table Layout/Layout Table/src/com/layout/tabel/Layout_Tabel.java
@@ -1,12 +1,18 @@
 package com.layout.tabel;
 
+// Project Layout_Table
+
+
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class Layout_Tabel extends Activity {
 	
-	//	Called when the activity is first created.
-
+    //	Called when the activity is first created.
+	
+	
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout__tabel);
