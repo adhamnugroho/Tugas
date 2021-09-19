f73ac111c98ebefb055b08e93c7c2b16b668569c
adhamnugroho <76804759+adhamnugroho@users.noreply.github.com>
2021-09-19 19:49:32 +0700

    Menambahkan repo tugas 7 dan beberapa file

---
+++ Tugas Web Dinamis/Tugas 7 - Desain Layout/Soal_1/src/com/belajar/Relatif_Layout_RPL_1.java
@@ -0,0 +1,15 @@
+package com.belajar;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Relatif_Layout_RPL_1 extends Activity {
+	
+	// Called when the activity is first created.
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatif__layout__rpl_1);
+    }
+}
---
+++ Tugas Web Dinamis/Tugas 7 - Desain Layout/Soal_2/src/com/belajar/Layout_Tabel.java
@@ -0,0 +1,15 @@
+package com.belajar;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Layout_Tabel extends Activity {
+	
+	// Called when the activity is first created.
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_layout__tabel);
+	}
+}
