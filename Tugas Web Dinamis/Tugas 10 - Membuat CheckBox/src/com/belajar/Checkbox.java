a34144acb74f4c2472b258c6102f73fc28e49356
Adham Nugroho <76804759+adhamnugroho@users.noreply.github.com>
2022-01-05 17:47:29 +0700

    Mengganti protected menjadi public
    Mengganti access modifier method OnCreate, yang sebelumnya protected menjadi public

--- Tugas Web Dinamis/Tugas 10 - Membuat CheckBox/src/com/belajar/Checkbox.java
+++ Tugas Web Dinamis/Tugas 10 - Membuat CheckBox/src/com/belajar/Checkbox.java
@@ -12,7 +12,7 @@ public class Checkbox extends Activity implements OnCheckedChangeListener{
 	CheckBox checkBox;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_checkbox);
 		
