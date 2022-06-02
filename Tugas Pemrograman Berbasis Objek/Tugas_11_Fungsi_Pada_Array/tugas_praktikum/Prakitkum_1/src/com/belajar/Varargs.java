8f5bfe9ba1f860e7a1192b6e7981f0655ad6c0f8
Adham Nugroho <76804759+adhamnugroho@users.noreply.github.com>
2022-06-02 18:57:37 +0700

    Deleted main method
    Deleted some code, from line 18 until line 34

--- Tugas Pemrograman Berbasis Objek/Tugas_11_Fungsi_Pada_Array/tugas_praktikum/Prakitkum_1/src/com/belajar/Varargs.java
+++ Tugas Pemrograman Berbasis Objek/Tugas_11_Fungsi_Pada_Array/tugas_praktikum/Prakitkum_1/src/com/belajar/Varargs.java
@@ -15,21 +15,4 @@ public class Varargs {
         
         System.out.println("\n");
     }
-    
-    
-    public static void main(String[] args) {
-        
-        // function with two argument int array
-        tampil("Daspro 2019", 100, 200);
-        
-        // function with five argument int array
-        tampil("Teknologi Informasi", 1, 2, 3, 4, 5);
-        
-        // function with zero argument int array
-        tampil("Polinema");
-        
-        // function with int array implisit in a variable
-        int angka[] = {1, 2, 3, 4};
-        tampil("Malang", angka);
-    }
 }
